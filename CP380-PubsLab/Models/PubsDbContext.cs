a3f28a256269ccae1d49d2e45e3e6c60f2f6f710
Aditya Papneja <apapneja@confederationcollege.ca>
2021-08-02 17:16:51 -0500

    Added Lab

--- CP380-PubsLab/Models/PubsDbContext.cs
+++ CP380-PubsLab/Models/PubsDbContext.cs
@@ -1,7 +1,8 @@
 ﻿using System;
 using Microsoft.EntityFrameworkCore;
 using System.IO;
-
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace CP380_PubsLab.Models
 {
@@ -14,10 +15,39 @@ namespace CP380_PubsLab.Models
         }
 
         // TODO: Add DbSets
+        public virtual DbSet<Employee> Employees { get; set; }
+        public virtual DbSet<Jobs> JobsList { get; set; }
+        public virtual DbSet<Titles> TitlesList { get; set; }
+        public virtual DbSet<Stores> StoresList { get; set; }
+        public virtual DbSet<Sales> SalesList { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             // TODO
+            modelBuilder.Entity<Employee>(x =>
+            {
+                x.HasIndex(e => e.emp_id);
+            });
+
+            modelBuilder.Entity<Jobs>(e =>
+            {
+                e.HasIndex(x => x.job_id);
+            });
+
+            modelBuilder.Entity<Sales>(e =>
+            {
+                e.HasIndex(x => x.ord_num);
+            });
+
+            modelBuilder.Entity<Titles>(e =>
+            {
+                e.HasIndex(x => x.pub_id);
+            });
+
+            modelBuilder.Entity<Stores>(e =>
+            {
+                e.HasIndex(x => x.stor_id);
+            });
         }
     }
 
@@ -25,27 +55,72 @@ namespace CP380_PubsLab.Models
     public class Titles
     {
         // TODO
+        [Key]
+        public string title_id { get; set; }
+        public string title { get; set; }
+        public string type { get; set; }
+        public int pub_id { get; set; }
+        public double price { get; set; }
+        public double advance { get; set; }
+        public int royalty { get; set; }
+        public int ytd_sales { get; set; }
+        public string notes { get; set; }
+        public DateTime pubdate { get; set; }
+        public Stores store { get; set; }
     }
 
 
     public class Stores
     {
         // TODO
+        [Key]
+        public int stor_id { get; set; }    
+        public string stor_name { get; set; }  
+        public string stor_address { get; set; }
+        public string city { get; set; }       
+        public string state { get; set; }      
+        public int zip { get; set; }
+        public List<Titles> Titles { get; set; }
     }
 
 
     public class Sales
     {
         // TODO
+        [Key]
+        public int stor_id { get; set; }
+        public string ord_num { get; set; }
+        public DateTime ord_date { get; set; }
+        public int qty { get; set; }
+        public string payterms { get; set; }
+        public string title_id { get; set; }
     }
 
     public class Employee
     {
         // TODO
+        [Key]
+        [Required]
+        public string emp_id { get; set; }
+        public string fname { get; set; }
+        public char minit { get; set; }
+        public string lname { get; set; }
+        public int job_id { get; set; }
+        public int job_lvl { get; set; }
+        public int pub_id { get; set; }
+        public DateTime hire_date { get; set; }
+        public Jobs currentJob { get; set; }
     }
 
     public class Jobs
     {
         // TODO
+        [Key]
+        [Required]
+        public int job_id { get; set; } 
+        public string job_desc { get; set; }
+        public int min_lvl { get; set; }
+        public int max_lvl { get; set; }
+        public ICollection<Employee> employees { get; set; }
     }
 }
--- CP380-PubsLab/Program.cs
+++ CP380-PubsLab/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace CP380_PubsLab
 {
@@ -10,17 +12,30 @@ namespace CP380_PubsLab
             {
                 if (dbcontext.Database.CanConnect())
                 {
-                    Console.WriteLine("Yes, I can connect");
+                    Console.WriteLine("Yes, I can connected");
                 }
 
                 // 1:Many practice
                 //
                 // TODO: - Loop through each employee
                 //       - For each employee, list their job description (job_desc, in the jobs table)
+                List<string> Jd = new List<string>();
+                var employees = dbcontext.Employees;
+                foreach(var emp in employees)
+                {
+                    Jd.Add(dbcontext.JobsList.Find(emp.emp_id).job_desc);
+                }
 
                 // TODO: - Loop through all of the jobs
                 //       - For each job, list the employees (first name, last name) that have that job
 
+                List<Tuple<string, string>> empDetails = new List<Tuple<string, string>>();
+                var jobs = dbcontext.JobsList;
+                foreach (var job in jobs)
+                {
+                    var temp = dbcontext.Employees.Find(job.job_id);
+                    empDetails.Add(new Tuple<string, string>(temp.fname, temp.lname));
+                }
 
                 // Many:many practice
                 //
@@ -29,12 +44,47 @@ namespace CP380_PubsLab
                 //
                 // e.g.
                 //  Bookbeat -> The Gourmet Microwave, The Busy Executive's Database Guide, Cooking with Computers: Surreptitious Balance Sheets, But Is It User Friendly?
-                
+
+                Dictionary<int, List<string>> titles = new Dictionary<int, List<string>>();
+                var stores = dbcontext.StoresList;
+                foreach(var st in stores)
+                {
+                    if(titles.ContainsKey(st.stor_id))
+                    {
+                        var temp = titles[st.stor_id];
+                        temp.AddRange(st.Titles.Select(x=> x.title).ToArray());
+                        titles[st.stor_id] = temp;
+                    }
+                    else
+                    {
+                        titles.Add(st.stor_id,new List<string>(st.Titles.Select(x=> x.title).ToArray()));
+                    }
+                }
+
+
                 // TODO: - Loop through each Title
                 //       - For each title, list all the stores it was sold at
                 //
                 // e.g.
                 //  The Gourmet Microwave -> Doc-U-Mat: Quality Laundry and Books, Bookbeat
+
+                Dictionary<string, List<string>> store = new Dictionary<string, List<string>>();
+                var titleList = dbcontext.TitlesList;
+                foreach (var title in titleList)
+                {
+                    if (store.ContainsKey(title.title_id))
+                    {
+                        var temp = store[title.title_id];
+                        temp.AddRange(titleList.Where(x => x.title_id == title.title_id).Select(x => x.store.stor_name).ToArray());
+                        store[title.title_id] = temp;
+                    }
+                    else
+                    {
+                        store.Add(title.title_id, new List<string>(titleList.Where(x => x.title_id == title.title_id).Select(x => x.store.stor_name).ToArray()));
+                    }
+                }
+
+                Console.ReadLine();
             }
         }
     }
